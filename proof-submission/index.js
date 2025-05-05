4358dafe2619a23a08b414d15ad78fad87f99fad
devEMEL <dominichenry882@gmail.com>
2025-05-05 13:31:01 +0100

    aggregation receipt

--- proof-submission/index.js
+++ proof-submission/index.js
@@ -1,6 +1,6 @@
 
 // import { CurveType, Library, zkVerifySession } from 'zkverifyjs';
-const {CurveType, Library, zkVerifySession} = require("zkverifyjs");
+const {CurveType, Library, zkVerifySession, ZkVerifyEvents} = require("zkverifyjs");
 const dotenv = require("dotenv");
 
 // const fs = require("fs");
@@ -12,17 +12,17 @@ dotenv.config();
 
 const main = async() => {
 
-    
+
     // const session = await zkVerifySession.start().Testnet().withAccount(process.env.ZKVERIFY_SEED_PHRASE);
     const session = await zkVerifySession
         .start()
-        .Testnet()
+        .Volta()
         .withAccount("rebuild match beyond top water bullet surge ecology asset hood noise wish"); // dummy account
 
 
     const { events, transactionResult } = await session
     .verify()
-    .groth16(Library.gnark, CurveType.bn128) 
+    .groth16({ library: Library.snarkjs, curve: CurveType.bn128})
     .execute({
         proofData: {
         vk:  vkey,
@@ -32,17 +32,44 @@ const main = async() => {
         domainId: 3, //op sepolia
     }); 
 
-    events.on('includedInBlock', (eventData) => {
-        console.log('Transaction included in block:', eventData);
-    });
+    // events.on('includedInBlock', (eventData) => {
+    //     console.log('Transaction included in block:', eventData);
+    // });
     
-    events.on('finalized', (eventData) => {
-        console.log('Transaction finalized:', eventData);
-    });
+    // events.on('finalized', (eventData) => {
+    //     console.log('Transaction finalized:', eventData);
+    // });
     
-    events.on('error', (error) => {
-        console.error('An error occurred during the transaction:', error);
-    });
+    // events.on('error', (error) => {
+    //     console.error('An error occurred during the transaction:', error);
+    // });
+    // events.on(ZkVerifyEvents.NewAggregationReceipt, (eventData) => {
+    //     console.error('An error occurred during the transaction:', error);
+    // });
+
+    session.subscribe([
+        {
+          event: ZkVerifyEvents.NewAggregationReceipt,
+          callback: (eventData) => {
+            console.log('Received NewAggregationReceipt event:', eventData);
+          },
+          options: {
+            domainId: 1
+          }
+        },
+        {
+          event: ZkVerifyEvents.ProofVerified,
+          callback: (eventData) => {
+            console.log('Proof verified successfully:', eventData);
+          }
+        },
+        {
+          event: ZkVerifyEvents.AggregationComplete,
+          callback: (eventData) => {
+            console.log('Aggregation process completed:', eventData);
+          }
+        }
+      ]);
     
     try {
         // Await the final transaction result
@@ -51,6 +78,8 @@ const main = async() => {
     
         // Log the final transaction result
         console.log('Transaction completed successfully:', transactionInfo);
+        const receipt = await session.waitForAggregationReceipt(transactionInfo.domainId, transactionInfo.aggregationId);
+        console.log({receipt});
       } catch (error) {
         // Handle any errors that occurred during the transaction
         console.error('Transaction failed:', error);
